7d4bdbf7f61117a34edeca49d8c8555f9dcd7fa5
Maxym Berezynets <halustyan@ukr.net>
2020-10-25 00:38:49 +0300

    поправил шаблоны

--- add.php
+++ add.php
@@ -11,17 +11,19 @@ $title = "Главная";
 $querycategories = "Select name, symbol_code from categories";
 $resultcategories = mysqli_query($con, $querycategories );
 $rowscategories= mysqli_fetch_all($resultcategories, MYSQLI_ASSOC);
-
-$add = include_template('add-lot.php', ['rowscategories'=>$rowscategories, 'is_auth' => $is_auth]);
-if (isset($submit)) {
+if (isset($_POST['senddata'])) {
     $required_fields = [$_POST['lot-name'], $_POST['category'], $_POST['message'], $_POST['lot-rate'], $_POST['lot-step']];
     $errors = [];
     foreach ($required_fields as $field) {
-        if (empty($_POST[$field])) {
+        if (empty($field)) {
             $errors[$field] = 'Поле не заполнено';
         }
     }
-    $errors["date"]  = is_date_valid($_POST['lot-date']);
+    if(!is_date_valid($_POST['lot-date'])){
+        $errors["date"] = 'Поле не заполнено';
+    }
 }
-print($add);
+$content = include_template('add-lot.php', ['rowscategories'=>$rowscategories, 'is_auth' => $is_auth]);
+$layout_content = include_template('layout.php', ['content' => $content, 'title' => 'Главная', 'rowscategories' => $rowscategories, 'is_auth' => $is_auth, 'user_name' => 'Максим Березинец']);
+print($layout_content);
 ?>
