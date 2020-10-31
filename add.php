e4291797ef7d710424254e1073b4d867824c3375
Maxym Berezynets <halustyan@ukr.net>
2020-10-31 22:46:50 +0200

    Правка 1.4 таска 6.2 Проблемы с выводо ошибки формы

--- add.php
+++ add.php
@@ -14,14 +14,39 @@ $errors = [];
 if (isset($_POST['senddata'])) {
     $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step'];
     foreach ($required_fields as $field) {
-        if (empty($_POST[$field])) {
-            $errors[$field] = 'Поле не заполнено';
+        if ($_POST[$field]) {
+            $errors['field'] = 'Поле не заполнено';
+        }
+        else {
+            unset($errors['field']);
         }
     }
-    if(!is_date_valid($_POST['lot-date'])){
-        $errors["date"] = 'Поле не заполнено';
+    if(!is_date_valid($_POST["lot-date"])){
+        $errors['date'] = 'Поле даты не валидно';
     }
 }
+if (isset($_FILES['add-lot-file'])) {
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $file_name = $_FILES['add-lot-file']['name'];
+    $file_path = __DIR__ . '/uploads/';
+    $file_url = '/uploads/' . $file_name;
+
+    move_uploaded_file($_FILES['add-lot-file']['tmp_name'], $file_path . $file_name);
+    $file_type = finfo_file($finfo, $file_name);
+    if ($file_type !== 'image/jpg'||$file_type !== 'image/jpeg'||$file_type !== 'image/png') {
+        $errors['format'] = "Неверный формат картинки";
+    }
+
+}
+
+if (!is_numeric($_POST['lot-rate'])&&($_POST['lot-rate']<0)){
+    $errors['lot-rate'] = "Начальная цена не число! Или меньше ноля";
+}
+
+if (!is_numeric($_POST['lot-step'])&&($_POST['lot-step']<0)){
+    $errors['lot-step'] = "Шаг ставки не число! Или меньше ноля";
+}
+
 $content = include_template('add-lot.php', ['rowscategories'=>$rowscategories, 'is_auth' => $is_auth, 'errors'=>$errors]);
 $layout_content = include_template('layout.php', ['content' => $content, 'title' => 'Главная', 'rowscategories' => $rowscategories, 'is_auth' => $is_auth, 'user_name' => 'Максим Березинец']);
 print($layout_content);
--- templates/add-lot.php
+++ templates/add-lot.php
@@ -1,4 +1,4 @@
-  <main>
+  <?php var_dump($errors); ?><main>
     <nav class="nav">
       <ul class="nav__list container">
       <?php
@@ -10,13 +10,13 @@
         <?php endforeach; ?>
       </ul>
     </nav>
-    <form class="form form--add-lot container <?php  if ($errors) { echo "form--invalid";}?>" action="add.php" method="post">
+    <form class="form form--add-lot container <?php  if ($errors) { echo "form--invalid";}?>" action="add.php" method="post" enctype="multipart/form-data">
       <h2>Добавление лота</h2>
       <div class="form__container-two">
         <div class="form__item <?php  if ($errors) { echo "form--invalid";}?>">
           <label for="lot-name">Наименование <sup>*</sup></label>
           <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required >
-          <span class="form__error"><?php  if ($errors[$field]) { echo $errors[$field];}?></span>
+          <span class="form__error"><?php  if ($errors['field']) { echo $errors['field'];}?></span>
         </div>
         <div class="form__item <?php  if ($errors) { echo "form__item--invalid";}?>">
           <label for="category">Категория <sup>*</sup></label>
@@ -34,32 +34,45 @@
       <div class="form__item form__item--wide <?php  if ($errors) { echo "form__item--invalid";}?>">
         <label for="message">Описание <sup>*</sup></label>
         <textarea id="message" name="message" placeholder="Напишите описание лота" required ></textarea>
-        <span class="form__error"><?php  if ($errors[$field]) { echo $errors[$field];}?></span>
+        <span class="form__error"><?php  if ($errors['field']) { echo $errors['field'];}?></span>
       </div>
       <div class="form__item form__item--file <?php  if ($errors) { echo "form__item--invalid";}?>">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
-          <input class="visually-hidden" type="file" id="lot-img" value="" required >
+          <input class="visually-hidden" type="file" id="lot-img" value="" required name="add-lot-file">
           <label for="lot-img">
             Добавить
           </label>
         </div>
+        <span class="form__error"><?php  if ($errors['format']) { echo $errors['format'];}?></span>
       </div>
       <div class="form__container-three">
         <div class="form__item form__item--small <?php  if ($errors) { echo "form__item--invalid";}?>">
           <label for="lot-rate">Начальная цена <sup>*</sup></label>
           <input id="lot-rate" type="text" name="lot-rate" placeholder="0" required >
-          <span class="form__error"><?php  if ($errors[$field]) { echo $errors[$field];}?></span>
+          <span class="form__error"><?php
+          if ($errors['field']) {
+              echo $errors['field']
+            };
+              if ($errors['lot-rate']) {
+                  echo $errors['lot-rate'];
+            }
+            ?>
+            </span>
         </div>
         <div class="form__item form__item--small <?php  if ($errors) { echo "form__item--invalid";}?>">
           <label for="lot-step">Шаг ставки <sup>*</sup></label>
           <input id="lot-step" type="text" name="lot-step" placeholder="0" required >
-          <span class="form__error"><?php  if ($errors[$field]) { echo $errors[$field];}?></span>
+          <span class="form__error"><?php  if ($errors['field']) { echo $errors['field'];}?></span>
         </div>
         <div class="form__item <?php  if ($errors) { echo "form__item--invalid";}?>">
           <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
           <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" required >
-          <span class="form__error"><?php  if ($errors[$field]) { echo $errors[$field];}?></span>
+          <span class="form__error"><?php
+            if ($errors['date']) {
+              echo $errors['date']; }
+                  ?>
+                  </span>
         </div>
       </div>
       <span class="form__error form__error--bottom"><?php  if ($errors) { echo "Исправьте ошибки в форме";}?></span>
