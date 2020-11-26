b50bcb8f5d27b434b8a9a6a6072100dd3299ffd4
Maxym Berezynets <halustyan@ukr.net>
2020-11-26 19:11:28 +0200

    наброски ауторизации

---
+++ registration.php
@@ -0,0 +1,60 @@
+<?php
+require_once("connection.php");
+require_once("helpers.php");
+require_once("function.php");
+
+$is_auth = rand(0, 1);
+$user_name = 'Максим Березинец';
+$title = "Главная";
+
+$querycategories = "Select id, name, symbol_code from categories";
+$resultcategories = mysqli_query($con, $querycategories );
+$rowscategories = mysqli_fetch_all($resultcategories, MYSQLI_ASSOC);
+
+$email = mysqli_real_escape_string($con, $_POST['email']??NULL);
+
+$password = mysqli_real_escape_string($con, $_POST['password']??NULL);
+
+$password = password_hash($password, PASSWORD_DEFAULT);
+
+$name  = mysqli_real_escape_string($con, $_POST['name']??NULL);
+
+$contacts = mysqli_real_escape_string($con, $_POST['message']??NULL);
+
+$errors = [];
+$register = $_POST['register']??NULL;
+
+if (isset($register)) {
+
+  if (empty($email)) {
+    $errors['email'] = "Не заполнено поле майла!";
+  }
+  $validateemail = filter_var($email, FILTER_VALIDATE_EMAIL);
+  if ($validateemail) {
+    $errors['wrongformatemail'] = "Неверный формат email";
+  }
+  if (empty($password)) {
+    $errors['password'] = "Не заполнено поле пароля!";
+  }
+  if (empty($name)) {
+    $errors['name'] = "Не заполнено поле имени!";
+  }
+   if  (empty($message)) {
+    $errors['message'] = "Не заполнено поле контактных данных!";
+  };
+  $check_email = "SELECT COUNT(`id`) as count FROM `users` WHERE `email` = ".$email;
+  $check_email_query = mysqli_query($con, $check_email );
+  $email_check_results = mysqli_fetch_array($check_email_query, MYSQLI_ASSOC);
+  if (count($email_check_results)>=1) {
+      $errors['repeatemail'] = "Такой email уже есть";
+  }
+}
+
+if (!$errors) {
+    $insertafterregistrate = "INSERT INTO users (email, password, name, contacts) VALUES ($email, $password, $name, $contacts)";
+    mysqli_query($con, $insertafterregistrate);
+}
+
+$content = include_template('registration-page.php', ['rowscategories' =>$rowscategories, "errors"=>$errors]);
+$layout_content = include_template('layout.php', ['content' => $content, "errors"=>$errors, 'is_auth' => $is_auth, 'user_name' => 'Максим Березинец', 'rowscategories' =>$rowscategories]);
+print($layout_content);
--- templates/add-lot.php
+++ templates/add-lot.php
@@ -16,13 +16,13 @@
 
         <div class="form__item <?php  if (isset($errors['name'])) { echo "form__item--invalid";}?>">
           <label for="lot-name">Наименование <sup>*</sup></label>
-          <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" >
+          <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
           <span class="form__error"><?php  if (isset($errors['name'])) { echo $errors['name'];}?></span>
         </div>
 
         <div class="form__item <?php  if (isset($errors['categories'])) { echo "form__item--invalid";}?>">
           <label for="category">Категория <sup>*</sup></label>
-          <select id="category" name="category"  >
+          <select id="category" name="category"  required>
           <option value="-1">Выберите категорию</option>
             <?php
                  foreach ($rowscategories as $row):
@@ -38,14 +38,14 @@
 
       <div class="form__item form__item--wide <?php  if (isset($errors['message'])) { echo "form__item--invalid";}?>">
         <label for="message">Описание <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите описание лота"  ></textarea>
+        <textarea id="message" name="message" placeholder="Напишите описание лота" required ></textarea>
         <span class="form__error"><?php  if (isset($errors['message'])) { echo $errors['message'];}?></span>
       </div>
 
       <div class="form__item form__item--file <?php  if (isset($errors['format'])) { echo "form__item--invalid";}?>">
         <label>Изображение <sup>*</sup></label>
         <div class="form__input-file">
-          <input class="visually-hidden" type="file" id="lot-img" value=""  name="add-lot-file">
+          <input class="visually-hidden" type="file" id="lot-img" value=""  name="add-lot-file" required>
           <label for="lot-img">
             Добавить
           </label>
@@ -58,7 +58,7 @@
       <div class="form__container-three">
         <div class="form__item form__item--small <?php  if (isset($errors['lot-rate-num'])||isset($errors['lot-rate-empty'])) { echo "form__item--invalid";}?>">
           <label for="lot-rate">Начальная цена <sup>*</sup></label>
-          <input id="lot-rate" type="text" name="lot-rate" placeholder="0" value="<?php  if (isset($errors['lot-rate-num'])) { echo $_POST['lot-rate'];}?>">
+          <input id="lot-rate" type="text" name="lot-rate" placeholder="0" required value="<?php  if (isset($errors['lot-rate-num'])) { echo $_POST['lot-rate'];}?>">
           <span class="form__error"><?php
           if (isset($errors['lot-rate-empty'])) {
               echo $errors['lot-rate-empty'];
@@ -72,7 +72,7 @@
         </div>
         <div class="form__item form__item--small <?php  if (isset($errors['lot-step-empty'])||isset($errors['lot-step-num'])) { echo "form__item--invalid";}?>">
           <label for="lot-step">Шаг ставки <sup>*</sup></label>
-          <input id="lot-step" type="text" name="lot-step" placeholder="0" value="<?php  if (isset($errors['lot-step-num'])) { echo $_POST['lot-step'];}?>">
+          <input id="lot-step" type="text" name="lot-step" required placeholder="0" value="<?php  if (isset($errors['lot-step-num'])) { echo $_POST['lot-step'];}?>">
           <span class="form__error"><?php
           if (isset($errors['lot-step-empty'])) {
               echo $errors['lot-step-empty'];
@@ -84,7 +84,7 @@
         </div>
         <div class="form__item <?php  if (isset($errors['wrongdate']) ) { echo "form__item--invalid";}?>">
           <label for="lot-date">Дата окончания торгов <sup>*</sup></label>
-          <input class="form__input-date" id="lot-date" type="text" name="lot-date" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?php  if (isset($errors['wrongdate'])) { echo $_POST['lot-date'];}?>" >
+          <input class="form__input-date" id="lot-date" type="text" name="lot-date" required placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?php  if (isset($errors['wrongdate'])) { echo $_POST['lot-date'];}?>" >
           <span class="form__error">
                   <?php if (isset($errors['wrongdate']) ){echo $errors['wrongdate'];} ?>
                   <?php if (isset( $errors['missing-date']) ){echo $errors['missing-date'];} ?>
