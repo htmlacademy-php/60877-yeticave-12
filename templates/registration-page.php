ec2e925122fea09ba42a5abaaf03a4f9f0e9988c
Maxym Berezynets <halustyan@ukr.net>
2020-11-28 11:21:43 +0200

    добавил оставление в форме

--- templates/registration-page.php
+++ templates/registration-page.php
@@ -20,7 +20,10 @@
 
 
         <label for="email">E-mail <sup>*</sup></label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail" >
+        <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?php if (isset($errors['repeatemail'])){ echo $_POST['email'];}?>
+        <?php if (isset($errors['wrongformatemail'])){ echo $_POST['email'];}?>
+        <?php if (isset($errors['repeatemail'])){ echo $_POST['email'];}?>
+        ">
         <span class="form__error">      <?php if (isset($errors['repeatemail'])){ echo $errors['repeatemail'];}?>
       <?php if (isset($errors['wrongformatemail'])){ echo $errors['wrongformatemail'];}?></span>
       </div>
@@ -31,12 +34,12 @@
       </div>
       <div class="form__item <?php if (isset($errors['name'])){ echo "form__item--invalid";}?>">
         <label for="name">Имя <sup>*</sup></label>
-        <input id="name" type="text" name="name" placeholder="Введите имя">
+        <input id="name" type="text" name="name" placeholder="Введите имя" value="<?php if (isset($errors['name'])){ echo $_POST['name'];}?>">
         <span class="form__error"><?php if (isset($errors['name'])){ echo $errors['name'];}?></span>
       </div>
       <div class="form__item <?php if (isset($errors['message'])){ echo "form__item--invalid";}?>">
         <label for="message">Контактные данные <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите как с вами связаться" ></textarea>
+        <textarea id="message" name="message" placeholder="Напишите как с вами связаться" value="<?php if (isset($errors['message'])){ echo $_POST['message'];}?>"></textarea>
         <span class="form__error"><?php if (isset($errors['message'])){ echo $errors['message'];}?></span>
       </div>
       <?php
